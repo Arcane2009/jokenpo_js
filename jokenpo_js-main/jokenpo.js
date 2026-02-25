b7510d9995a693f6cd61b8e87ee33bd02ff6237b
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-02-25 09:55:11 -0300

    Add files via upload

---
+++ jokenpo_js-main/jokenpo.js
@@ -0,0 +1,48 @@
+function jogar(){
+
+    if(document.getElementById("pedra").checked == false && 
+        document.getElementById("papel").checked == false && 
+        document.getElementById("tesoura").checked == false){
+
+        alert("Selecione uma função")
+    }
+    else{
+
+        let sorteio = Math.floor(Math.random()*3);
+        
+        switch(sorteio){
+
+            case 0:
+                document.getElementById("pc").src="pcpedra.png";
+                break;
+
+            case 1:
+                document.getElementById("pc").src="pcpapel.png";
+                break;
+            
+            case 2:
+                document.getElementById("pc").src="pctesoura.png";
+                break;
+        }
+    
+    if((document.getElementById("pedra").checked == true && sorteio == 0)
+        ||(document.getElementById("papel").checked == true && sorteio == 1)
+        ||(document.getElementById("tesoura").checkde == true && sorteio == 2))
+        {
+                document.getElementById("placar").innerHTML = "EMPATE!";
+    }
+    else if((document.getElementById("pedra").checked == true && sorteio == 2)
+        ||(document.getElementById("papel").checked == true && sorteio == 0)
+        ||(document.getElementById("tesoura").checked == true && sorteio == 1))
+        {
+                document.getElementById("placar").innerHTML = "EU PERDI!";
+    }
+    else{
+        document.getElementById("placar").innerHTML = "EU GANHEI HAHAHAHA";
+    }
+    }
+}
+function resetar(){
+    document.getElementById("pc").src = "pc.png";
+    document.getElementById("placar").innerHTML = "";
+}
